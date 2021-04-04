052eb354f2a0a40b10fdc1f76cf689865203b495
Shantnu12345 <shantnukakkar1992@gmail.com>
2021-04-04 02:14:15 -0400

    Added accounts class

---
+++ src/Account.cpp
@@ -0,0 +1,53 @@
+#include "Account.h"
+
+inline string toString(AccountType accountType)
+{
+
+  switch (accountType)
+  {
+  case Checking:
+    return "Checking";
+
+  case Savings:
+    return "Savings";
+  }  
+  return "Checking";
+}
+
+bool Account::addAccountType(AccountType accountType, unsigned int newBalance) 
+{
+    _balances[toString(accountType)] = newBalance;
+    return true;
+}
+
+bool Account::checkAccountTypeExists(AccountType accountType) const
+{
+    return _balances.find(toString(accountType)) != _balances.end();
+}
+
+bool Account::updateBalance(AccountType accountType, unsigned int newBalance) 
+{
+  cout<<" h2 ";
+    if(!checkAccountTypeExists(accountType))
+        return false;
+
+  cout<<newBalance;
+    _balances[toString(accountType)] = newBalance;
+  cout<<" h3 ";
+
+    return true;
+}
+
+//Make sure to check account exists before calling this function
+unsigned int Account::getBalance(AccountType accountType) const
+{
+    if(!checkAccountTypeExists(accountType))
+        return 0;
+
+    return _balances.at(toString(accountType));
+}
+
+string Account::getPin() const
+{
+    return _pin;
+}
---
+++ src/Account.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+enum AccountType
+{
+    Checking,
+    Savings
+};
+
+class Account
+{
+public:
+    Account(string cardNum, string pin) : _cardNum(cardNum), _pin(pin), _balances() {}
+    bool addAccountType(AccountType accountType, unsigned int newBalance);
+    bool checkAccountTypeExists(AccountType accountType) const;
+    bool updateBalance(AccountType accountType, unsigned int newBalance);
+    unsigned getBalance(AccountType accountType) const;
+    string getPin() const;
+
+protected:
+    string _cardNum;
+    string _pin;
+    unordered_map<string, unsigned int> _balances; //Maps from account type (example checking, savings) to balance info
+                                                //For simplicity it is assumed that a account type doesnt exist if 
+                                                //it is not present in _balances
+};
